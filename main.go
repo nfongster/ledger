9c030c9625e66511f1595066d04a2f3ed7634681
Nick <nsfong79@gmail.com>
2025-08-29 19:34:25 -0700

    Set up REPL skeleton and a single command (quit)

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello there!")
+	fmt.Println("Welcome to ledger!")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		str, _ := reader.ReadString('\n')
+		input := strings.Split(str, " ")
+
+		cmd, args := input[0], input[1:]
+		command, exists := GetCommand(cmd)
+		if !exists {
+			fmt.Println("Command does not exist!")
+		} else {
+			command.Execute(args...)
+		}
+	}
+}
+
+type Command struct {
+	Name        string
+	Description string
+	Callback    func(...string)
+}
+
+func (c Command) Execute(args ...string) {
+	c.Callback(args...)
+}
+
+func GetCommand(command string) (*Command, bool) {
+	cmd := strings.TrimSpace(strings.ToLower(command))
+	switch cmd {
+	case "quit":
+		return &Command{
+			Name:        "quit",
+			Description: "Exit the program.",
+			Callback: func(args ...string) {
+				fmt.Println("Exiting...")
+				os.Exit(0)
+			},
+		}, true
+
+	default:
+		return nil, false
+	}
 }
