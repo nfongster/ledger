2355ae250c20eed8903e2831642cf32ba88a7dfc
Nick <nsfong79@gmail.com>
2025-09-02 18:53:18 -0700

    Move commands from switch statement to map

--- internal/commands/command.go
+++ internal/commands/command.go
@@ -5,7 +5,6 @@ import (
 )
 
 type Command struct {
-	Name        string
 	Description string
 	callback    func(...string)
 }
@@ -14,31 +13,23 @@ func (c Command) Execute(args ...string) {
 	c.callback(args...)
 }
 
+var commandMap map[string]Command = map[string]Command{
+	"quit": {
+		Description: "Exit the program.",
+		callback:    quit,
+	},
+	"transactions": {
+		Description: "Get a list of all available transactions, or supply an ID as an argument to get a single transaction.",
+		callback:    transactions,
+	},
+	"add": {
+		Description: "Add a new entry to the ledger.  Args: date, desc, amount, cat, notes",
+		callback:    add,
+	},
+}
+
 func GetCommand(command string) (*Command, bool) {
-	cmd := strings.TrimSpace(strings.ToLower(command))
-	switch cmd {
-	case "quit":
-		return &Command{
-			Name:        "quit",
-			Description: "Exit the program.",
-			callback:    quit,
-		}, true
-
-	case "transactions":
-		return &Command{
-			Name:        "transactions",
-			Description: "Get a list of all available transactions, or supply an ID as an argument to get a single transaction.",
-			callback:    transactions,
-		}, true
-
-	case "add":
-		return &Command{
-			Name:        "add",
-			Description: "Add a new entry to the ledger.  Args: date, desc, amount, cat, notes",
-			callback:    add,
-		}, true
-
-	default:
-		return nil, false
-	}
+	command = strings.TrimSpace(strings.ToLower(command))
+	cmd, exists := commandMap[command]
+	return &cmd, exists
 }
